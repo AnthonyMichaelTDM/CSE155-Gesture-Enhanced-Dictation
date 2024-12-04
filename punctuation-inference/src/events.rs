0cda5efd62b09fabbf948cdf10177902cbb55afb
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-12-04 12:05:43 -0800

    fix(events): unify stop recording event naming to "StopRecording"

--- punctuation-inference/src/events.rs
+++ punctuation-inference/src/events.rs
@@ -180,7 +180,10 @@ mod serialization_tests {
     )]
     #[case::control_event_start(r#""Start""#, Event::Control(ControlEvent::Start))]
     #[case::control_event_stop(r#""Stop""#, Event::Control(ControlEvent::Stop))]
-    #[case::control_event_stop_recording(r#""Stop Recording""#, Event::Control(ControlEvent::StopRecording))]
+    #[case::control_event_stop_recording(
+        r#""StopRecording""#,
+        Event::Control(ControlEvent::StopRecording)
+    )]
     #[case::control_event_reset(r#""Reset""#, Event::Control(ControlEvent::Reset))]
     #[case::control_event_exit(r#""Exit""#, Event::Control(ControlEvent::Exit))]
     fn test_deserialization(#[case] value: &str, #[case] expected: Event) {
@@ -216,7 +219,10 @@ mod serialization_tests {
     )]
     #[case::control_event_start(Event::Control(ControlEvent::Start), r#""Start""#)]
     #[case::control_event_stop(Event::Control(ControlEvent::Stop), r#""Stop""#)]
-    #[case::control_event_stop_recording(Event::Control(ControlEvent::StopRecording), r#""Stop Recording""#)]
+    #[case::control_event_stop_recording(
+        Event::Control(ControlEvent::StopRecording),
+        r#""StopRecording""#
+    )]
     #[case::control_event_reset(Event::Control(ControlEvent::Reset), r#""Reset""#)]
     #[case::control_event_exit(Event::Control(ControlEvent::Exit), r#""Exit""#)]
     fn test_serialization(#[case] event: Event, #[case] expected: &str) {
